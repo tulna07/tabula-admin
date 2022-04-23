327eaed021527974708d70dd30f9ffc369b4eb48
scoobytux <tu.lna07@gmail.com>
2022-04-23 19:44:20 +0700

    Update user.js

---
+++ app/models/user.js
@@ -0,0 +1,17 @@
+function User(
+  _account,
+  _fullName,
+  _password,
+  _email,
+  _userType,
+  _language,
+  _description
+) {
+  this.account = _account;
+  this.fullName = _fullName;
+  this.password = _password;
+  this.email = _email;
+  this.userType = _userType;
+  this.language = _language;
+  this.description = _description;
+}
